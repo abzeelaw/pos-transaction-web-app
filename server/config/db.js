f09c5fde47be785978f8625db9020ea1eafea3cc
abzeelaw <habeeblawal2020@gmail.com>
2026-08-18 17:16:34 +0100

    Prepare full stack app for Vercel

--- server/config/db.js
+++ server/config/db.js
@@ -2,12 +2,20 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    if (mongoose.connection.readyState === 1) {
+      return;
+    }
+
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB connected successfully");
   } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
-    process.exit(1);
+    console.error(
+      "MongoDB connection failed:",
+      error.message
+    );
+
+    throw error;
   }
 };
 
