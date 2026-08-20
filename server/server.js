96330544b8c895580eda1a2fe787e34237fc3bee
abzeelaw <habeeblawal2020@gmail.com>
2026-08-20 16:13:40 +0100

    fix backend deployment

--- server/server.js
+++ server/server.js
@@ -21,17 +21,16 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/transactions", transactionRoutes);
 
-// Connect to MongoDB
 connectDB();
 
-// Export Express app for Vercel
 export default app;
 
-// Local development server
 if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 5000;
 
   app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(
+      `Server running on port ${PORT}`
+    );
   });
 }
